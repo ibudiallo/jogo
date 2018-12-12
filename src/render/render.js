b110e499ddbfe357b2db1295b63a5a77dfbb413e
Ibrahim Diallo <ibuvdiallo@gmail.com>
2018-12-11 16:26:38 -0800

    adding support for xml (#1)
    adding support for xml

--- src/render/render.js
+++ src/render/render.js
@@ -22,6 +22,8 @@ var Render = {
 		this.convBtn = document.getElementById( "convBtn" );
 		this.optionBtn = document.getElementById( "option-btn" );
 		this.textField.value = JSON.stringify( sample, null,"  " );
+		// TODO remove
+		this.textField.value = xmlsample;
 		this.convBtn.onclick = function() {
 			Render.update();
 		};
@@ -61,9 +63,11 @@ var Render = {
 		Jogo.opts = {
 			tabChar: this.form.tabs.value === "tab" ? "\t" : "    ",
 			annotation: this.form.annotation.checked,
-			omitempty: this.form.omitempty.checked
+			omitempty: this.form.omitempty.checked,
+			newLine: "\n"
 		};
 		this.tabChar = Jogo.opts.tabChar;
+		console.log(Jogo.opts);
 		this.update();
 	},
 
